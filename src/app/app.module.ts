dea6251f0361d97cb83c0c077a51dfc2db02fb0a
Satwik Tatikonda <satwiktatikonda@Satwiks-MacBook-Pro.local>
2024-11-12 12:32:24 +0530

    component setup into standalone and non-standalone(module)

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { FormsModule } from '@angular/forms';
+
+// for standalone components we use imports bootstrap for module components
+@NgModule({
+
+    declarations: [AppComponent,
+        HeaderComponent,
+        UserComponent,
+        CardComponent,
+        TasksComponent,
+        TasksComponent,
+        NewTaskComponent],
+    bootstrap: [AppComponent],
+    imports:[BrowserModule,FormsModule]
+
+})
+export class AppModule{
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// ****** way-1 ******
+// this is used to start application if the root Component is a standalone component
 
-import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// import { bootstrapApplication } from '@angular/platform-browser';
+
+// import { AppComponent } from './app/app.component';
+// import { Component } from '@angular/core';
+
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+
+
+// ****** way-2 ******
+// this is used to start application if the root Component is not a standalone component
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
